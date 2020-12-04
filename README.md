6690ae1318f33f4ff71e0d75348e29daf41c83ea
Beaver Bryan Antipolo <beaverbryanantipolo@gmail.com>
2020-12-03 20:57:30 -0500

    currently on work

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Calculator_JS -- What to expect?
 The title itself is self-explanatory, it is a project that synthesized the functionality of the combined js functions, featuring styles and the structured html to design a calculator in a website.
-
 ### The implementation of the custom evaluate function
-When digits are clicked, it'll be stored inside an array followed by the operator, next value and so on. Once the equal button is clicked, 
--- script.js
+++ script.js
@@ -4,15 +4,28 @@ The functions it consist works together to self-build a on-screen calculator.
 
 Written by: Beaver Bryan Antipolo
 Start Date: Dec 02 2020
-Last Modification Date: Dec 02 2020*/
+Last Modification Date: Dec 02 2020
 
+*/
+
+/* 
+
+Pseudocode (xxx) -- Difficulty and complexity by number of tabs
+(x)Declare multiple variables to be used: firstVal, secondVal, total, and op.
+(x)Declare an empty array that'll be reference as a print display for calculator screen.
+()Create a simple getDigits and getOp function. Make sure it returns the total input.
+()Enable the functionality of the "all clear" and "plus-add" button.
+    ()
+
+*/
 'use strict';
 /* global variables*/
 let arithmeticExpression = [];
-let arExp = {};
+//let arExp = {};
 let total = 0;
 let firstVal = '';
-let secondValue = '';
+let secondVal = '';
+let op = '';
 
 /*DOM Elements*/
 const allClear = document.getElementById('allClear');
@@ -25,52 +38,93 @@ const modulus = document.getElementById('remainder');
 const equal = document.getElementById('equal');
 const calcDisplay = document.getElementById('calc_display');
 const calcInput = document.getElementById('calc_input');
-const op = Array.from(document.querySelectorAll(".operators"));
+const opArray = Array.from(document.querySelectorAll(".operators"));
 const digitNodeList = [...document.querySelectorAll(".digits")];
 
+/*
 //functions inside the calculator function
 function calculator() {
     let op = '';
     digitInput();
     getOp(op);
     resetAllVar();
-    //populateDisplay();
-    total = customEval(op);
+    calcInput.textContent = customEval(op);
+    calcDisplay.textContent = printDisplay();
 }
-const getOp = (ope) => {
-    op.forEach(btn => btn.addEventListener("click", () => {
-        ope = btn.textContent;
-        calcDisplay.textContent += ope;
-        return ope;
-    }));
+
+function printDisplay(){
+    return arithmeticExpression.map((v,i,arr)=>{
+        return arr[i];
+    }).join(" ");
 }
-// function populateDisplay(){
-//     arithmeticExpression.forEach(e => )
-// }
 function customEval(op) {
     equal.onclick = () => {
+
+        arithmeticExpression.push(ope);
         return parseInt(firstVal) + op + parseInt(secondVal);
     }
 }
+
+function simpleExpression(){
+    if (firstVal !== undefined){
+        arithmeticExpression.push(firstVal);
+    }
+    if (secondVal !== undefined)
+
+    arithmeticExpression.push(ope);
+    arithmeticExpression.push(secondVal);
+}
+function resetAllVar() {
+    allClear.onclick = () => {
+        // calcInput.textContent = '';
+        // calcDisplay.textContent = '';
+        // firstVal = '';
+        arithmeticExpression = [];
+    }
+}
+*/
 //It reads the input of the button text content when it is being clicked 
-function digitInput(op) {
+function getDigits() {
     digitNodeList.forEach(btn => btn.addEventListener("click", () => {
-        calcInput.textContent += btn.textContent;
-        firstVal += calcInput.textContent;
+        let tempVal="";
+        if(firstVal == "" && op == ""){
+            firstVal += btn.textContent;
+            tempVal = firstVal;
+        }else{
+            secondVal += btn.textContent;
+            tempVal = secondVal;
+        }
+        arithmeticExpression.push(tempVal);
         calcDisplay.textContent += btn.textContent;
     }));
-    if (op !== undefined){
-        calcInput.textContent = '';
-    }
-    arithmeticExpression.push(firstVal);
+    //arithmeticExpression.push(firstVal);
 }
 function resetAllVar() {
     allClear.onclick = () => {
         calcInput.textContent = '';
         calcDisplay.textContent = '';
         firstVal = '';
+        arithmeticExpression = [];
     }
 }
-
-
-calculator();
+const getOp = () => {
+    opArray.forEach(btn => btn.addEventListener("click", () => {
+        op = btn.value;
+        calcDisplay.textContent += op;
+        arithmeticExpression.push(op);
+    }));
+}
+function customEval() {
+    let arr = arithmeticExpression.map((v) => {
+        return !isNaN(v) ? parseInt(v) : v;
+    });
+    equal.onclick = () => {
+        return arr.reduce()
+    }
+}
+getDigits();
+getOp();
+resetAllVar();
+total = customEval(op);
+calcInput.textContent = total;
+//calculator();
